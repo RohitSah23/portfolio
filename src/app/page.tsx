60e8626895e57e6f6197bced868e56aa91148aa4
RohitSah23 <rohitisog@gmail.com>
2025-10-08 22:46:56 +0530

    fix(Layout): fix meta log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
       <h1 className="text-4xl font-bold animate-bounce text-blue-600">
-        Coming Soon
+      <Button >
+         Coming Soon
+        </Button>
       </h1>
     </div>
   );
